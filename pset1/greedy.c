9ca2549bc1c643aa4e55ca92a0d7f76a6758e792
Marek <33625074+mkszymkun@users.noreply.github.com>
2017-11-17 16:45:17 +0100

    minor update

--- pset1/greedy.c
+++ pset1/greedy.c
@@ -1,23 +1,24 @@
-#include <cs50.h>
 #include <stdio.h>
+#include <cs50.h>
 #include <math.h>
 
 int main(void)
 {
-    printf("O hai! ");
-    float x;
-    do
+    printf("O hai! How much change is owed?\n");
+    float c = get_float();
+
+    while (c < 0)
     {
         printf("How much change is owed?\n");
-        x = get_float();
+        c = get_float();
     }
-    while (x < 0);
 
-    float c = roundf(x * 100);
-    int y = c;
-    int q = y / 25;
-    int d = y % 25 / 10;
-    int n = y % 25 % 10 / 5;
-    int p = y % 25 % 10 % 5 / 1;
+    int ch = roundf(c * 100);
+
+    int q = ch / 25;
+    int d = ch % 25 / 10;
+    int n = ch % 25 % 10 / 5;
+    int p = ch % 25 % 10 % 5 / 1;
+
     printf("%i\n", q + d + n + p);
 }
