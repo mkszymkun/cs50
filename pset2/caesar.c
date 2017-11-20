ef3ab381c9b7eb37182a88870f30ec52a409d0d8
Marek <33625074+mkszymkun@users.noreply.github.com>
2017-11-20 10:06:49 +0100

    Update caesar.c

--- pset2/caesar.c
+++ pset2/caesar.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <ctype.h>
 #include <cs50.h>
 #include <string.h>
 
@@ -7,45 +6,34 @@ int main(int argc, string argv[])
 {
     if (argc != 2)
     {
-        printf("INVALID NUMBER OF ARGUMENTS\n");
+        printf("ERROR\n");
         return 1;
     }
-    else
+
+    int k = atoi(argv[1]);
+
+    printf("plaintext:  ");
+    string plain = get_string();
+    int l = strlen(plain);
+
+
+    printf("ciphertext: ");
+
+    for (int i = 0; i < l; i++)
     {
-        printf("plaintext:  ");
-        string p = get_string();
-        printf("ciphertext: ");
-        int k = atoi(argv[1]);
-        int a;
-        int b;
-        int c;
-
-        for (int i = 0, l = strlen(p); i < l; i++)
+        if ((int) plain[i] > 64 && (int) plain[i] < 91)
         {
-
-            if ((int) p[i] > 64 && (int) p[i] < 91)
-            {
-                a = (int) p[i] - 65;
-                b = (a + k) % 26;
-                c = b + 65;
-                printf("%c", (char) c);
-            }
-
-            else if ((int) p[i] > 96 && (int) p[i] < 123)
-            {
-                a = (int) p[i] - 97;
-                b = (a + k) % 26;
-                c = b + 97;
-                printf("%c", (char) c);
-            }
-
-            else
-            {
-                printf("%c", p[i]);
-            }
+            int c = (((int) plain[i] - 65) + k) % 26;
+            plain[i] = c + 65;
         }
 
-        printf("\n");
-        return 0;
+        else if ((int) plain[i] > 96 && (int) plain[i] < 123)
+        {
+            int c = (((int) plain[i] - 97) + k) % 26;
+            plain[i] = c + 97;
+        }
     }
+
+    printf("%s\n", plain);
+    return 0;
 }
